32a359b20781cba38933e8507b4f3ad3606afd0e
Erik Dubois <erik.dubois@gmail.com>
2022-12-07 20:51:10 -0500

    changes to app

---
+++ helper.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import os
+import io
+from gurobipy import Model, GRB, quicksum
+import numpy as np
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import pandas as pd
+
+def solve(probabilities, ebec_input, prob_data):
+    mileages = {'7774': 105.9, '7773': 167.3, '7772': 145.9, '7771': 107.0, '7072': 112.1}
+    coach_names = sorted(list(ebec_input['Vehicle'].unique()))
+    coaches = {idx: route for idx, route in enumerate(coach_names)}
+    bus_iter = range(len(coaches.keys()))
+
+    block_names = sorted(mileages.keys())
+    blocks = {idx: block for idx, block in enumerate(block_names)}
+    block_iter = range(len(blocks.keys()))
+
+    model = Model('EDis')
+    p_j = np.reshape(probabilities.to_records(), (len(bus_iter), len(block_iter)))
+    vars = model.addVars(bus_iter, block_iter, vtype=GRB.BINARY, name='X')
+    model.addConstrs(vars.sum('*', j) <= 1 for j in block_iter)  # each block can only be assigned to one coach
+    model.addConstrs(vars.sum(i, '*') <= 1 for i in bus_iter)  # each coach can only be assigned to one block
+    model.addConstrs(vars[i, j] * 0.95 <= p_j[i, j][-1] for i in bus_iter for j in block_iter)  # probability constraint
+    model.modelSense = GRB.MAXIMIZE
+
+    z = (quicksum(p_j[i, j][-1] * vars[i, j] for i in bus_iter for j in block_iter))
+    model.setObjective(z)
+    model.optimize()
+    model.getObjective().getValue()
+
+    solution_vars = []
+    for v in model.getVars():
+        solution_vars.append(v.x)
+    solution_vars = np.reshape(np.abs(solution_vars), (len(bus_iter), len(block_iter)))
+
+    solution_vars = pd.DataFrame(solution_vars, index=coach_names, columns=block_names)
+    solution_vars = solution_vars.melt(ignore_index=False, var_name='Block')
+    solution_vars = solution_vars[solution_vars['value'] == 1]
+
+    return solution_vars
+
+
+
+def download():
+    creds = None
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
+
+        file_id = '1PZvEfJ0CDBDxuSQZOocr9a7ONPAwxqom'
+
+        request = service.files().get_media(fileId=file_id)
+        file = io.FileIO('df.pkl', mode='wb')
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(F'Download {int(status.progress() * 100)}.')
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+
+    return file
--- stream.py
+++ stream.py
@@ -1,77 +1,29 @@
-import requests
-from googleapiclient.http import MediaIoBaseDownload
+import math
+import gurobipy
 
-import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-import pickle
+import streamlit as st
 from pgbm import PGBM
-import altair as alt
 from scipy.stats import norm
-import os
-import io
 
-# get the file from the google drive
-from google.oauth2.credentials import Credentials
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from helper import solve, download
 
-# Set the file ID of the file you want to download
-file_id = 'YOUR_FILE_ID'
-def upload_basic():
-    """Insert new file.
-    Returns : Id's of the file uploaded
-
-    Load pre-authorized user credentials from the environment.
-    # TODO(developer) - See https://developers.google.com/identity
-    for guides on implementing OAuth2 for the application.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    SCOPES = ['https://www.googleapis.com/auth/drive']
-
-    if os.path.exists('../token.json'):
-        creds = Credentials.from_authorized_user_file('../token.json', SCOPES)
-    try:
-        # create drive api client
-        service = build('drive', 'v3', credentials=creds)
-
-        file_id = '1PZvEfJ0CDBDxuSQZOocr9a7ONPAwxqom'
-
-        # pylint: disable=maybe-no-member
-        request = service.files().get_media(fileId=file_id)
-        # file = io.BytesIO()
-        file = io.FileIO('df.pkl', mode='wb')
-        downloader = MediaIoBaseDownload(file, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print(F'Download {int(status.progress() * 100)}.')
-
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-        file = None
-
-
-
-    return file
-
-
-
-# Upload file
-file = upload_basic()
-# print(file)
-
+# download the file
+file = download()
 
 # setup the data
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(initial_sidebar_state="collapsed")
+st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
 st.title("E-BUS Data Portal")
 mileages = {'7774': 105.9, '7773': 167.3, '7772': 145.9, '7771': 107.0, '7072': 112.1}
 
 df = pd.read_pickle('df.pkl')
+
+prob_data = pd.DataFrame(columns=['coach', 'block', 'Probability of Block Completion', 'prob'])
 
 # Using "with" notation
 with st.sidebar:
@@ -85,37 +37,35 @@ with st.sidebar:
         ['Green', 'Yellow', 'Red', 'Blue'])
 
 coaches = df.vehicle.value_counts()
-
 ebec_input = pd.DataFrame(columns=['Date', 'Vehicle', 'start_per'])
 
 for coach in coaches.index:
     # grab most recent data
     coach_df = df[df.vehicle == coach]
     row = coach_df[coach_df.Date == coach_df.Date.max()].iloc[0, :]
-    # print(row['Date'])
-    #
     ebec_input = ebec_input.append({'Date': row['Date'], 'Vehicle': row['vehicle'], 'start_per': row['soc']},
                                    ignore_index=True)
 
 ebec_input['month'] = ebec_input['Date'].dt.month
 
 # setup tabs for output
-tab1, tab2 = st.tabs(['Today', 'Historical'])
+tab1, tab2, tab3, tab4= st.tabs(['Real Time Data and Predictions', 'Weekly Charging Optimization',
+                                 'Weekly Summary', 'Explanations'])
+                            # 'Historical'
 with tab1:
     # write output to dashboard
     output = ebec_input
-    output.start_per = output.start_per.astype('float64')
-    output.start_per = round(output.start_per, 1)
+    output.start_per = (np.around(output.start_per.astype(float), 0)).astype(int)
     output.Date = output.Date.dt.strftime("%m/%d %I:%M%P")
     output = output.rename(columns={'start_per': "SOC %", "Date": "Last Online"})
-
+    st.markdown("# Current SOC")
     cols = st.columns(2)
     with cols[0]:
         # st.bar_chart(data=output, y="Vehicle:N", x='SOC %:Q')
 
         bar = alt.Chart(output).mark_bar(color='green').encode(
             x='SOC %:Q',
-            y=alt.Y('Vehicle:N')
+            y=alt.Y('Vehicle:N', sort='ascending'),
         ).properties(
             height=alt.Step(40)  # controls width of bar.
         )
@@ -123,9 +73,10 @@ with tab1:
         st.altair_chart(bar, use_container_width=True)
     output.index = output.Vehicle
     with cols[1]:
-        st.dataframe(output[['SOC %', "Last Online"]])
+        output['SOC %'] = output['SOC %'].astype(str) + '%'
+        st.dataframe(output[['SOC %', "Last Online"]].sort_index(ascending=True))
 
-    #
+    # prepare data
     ebec_final = ebec_input[['start_per', 'month']]
     ebec_final['start_per'] = ebec_final['start_per'].astype(float)
 
@@ -139,9 +90,6 @@ with tab1:
     means = yhat_dist[1]
     variance = yhat_dist[2]
 
-    mean = means.mean()
-    var = variance.mean()
-
     predictions = []
     for coaches in range(ebec_final.shape[0]):
         min = yhat_dist[0][:, coaches].min().item()
@@ -150,13 +98,15 @@ with tab1:
 
     coaches = list(ebec_input.Vehicle.values)
 
-    preds_df = pd.DataFrame(columns=['coach', 'block', 'pred_eff', 'pred_miles', 'type'])
+    preds_df = pd.DataFrame(columns=['coach', 'block', 'pred_eff', 'pred_kwh', 'type'])
 
     for idx, coach in enumerate(coaches):
         pred = predictions[idx]
 
         low = pred[0]
         mid = yhat_point[idx].item()
+        mean = means[idx].item()
+        var = variance[idx].item()
         high = pred[1]
         preds = [low, mid, high]
 
@@ -171,43 +121,48 @@ with tab1:
                     type = 'high'
 
                 preds_df = preds_df.append(
-                    {'coach': coach, 'block': block, 'pred_eff': pred, 'pred_miles': pred * miles, 'type': type},
+                    {'coach': coach, 'block': block, 'pred_eff': pred, 'pred_kwh': pred * miles, 'type': type,
+                     'mean': mean, 'var': var},
                     ignore_index=True)
 
     fig, ax = plt.subplots(1, len(coaches), figsize=(10, 40))
     N_series = 3
     coaches = sorted(coaches)
 
-    st.header("Energy Consumption Predictions")
+    st.markdown("# Energy Consumption Predictions")
 
     n_coaches = ebec_final.shape[0]
 
     preds_df.type = pd.Categorical(preds_df.type,
                                    categories=["high", "mid", "low"],
                                    ordered=True)
-    nrows = int(n_coaches / 2)
+    nrows = math.ceil(n_coaches / 2)
     preds_df = preds_df.sort_values('type')
 
     for j in range(nrows):
         cols = st.columns(2)
         for idx, col in enumerate(cols):
             with col:
-                coach = coaches[idx + j * 2]
+                try:
+                    coach = coaches[idx + j * 2]
+                except IndexError:
+                    break
                 data = preds_df[preds_df["coach"] == coach]
-                st.header("Coach: {}".format(coach))
+                st.markdown("## Coach: {}".format(coach))
 
                 colors = list(sns.color_palette())
 
                 chart = alt.Chart(data).mark_bar(opacity=0.6).encode(
                     x='block',
-                    y=alt.Y('pred_miles', stack=None, sort='ascending'),
+                    y=alt.Y('pred_kwh', stack=None, sort='ascending',
+                            scale=alt.Scale(domain=[0, 500])),
                     color=alt.Color('type', scale=alt.Scale(
                         domain=['high', 'low', 'mid'],
                         range=["#4878D0", "#EE854A", "#6ACC64"]))
                 )
 
-                current_soc = float(ebec_input[ebec_input['Vehicle'] == coach]['start_per'].values[0]/100)
-                val = 440*(current_soc - 0.2)
+                current_soc = float(ebec_input[ebec_input['Vehicle'] == coach]['start_per'].values[0] / 100)
+                val = 440 * (current_soc - 0.2)
                 line = alt.Chart(pd.DataFrame({'y': [val]})).mark_rule(strokeDash=[5, 1], color="red").encode(y='y')
                 st.altair_chart(chart + line, use_container_width=True)
                 # st.write(norm.ppf())
@@ -216,20 +171,55 @@ with tab1:
                 mids = preds_df.query("type == 'mid' and coach == @coach")
                 # st.dataframe(mids)
 
-
-                for val in mids:
-                    st.write(val)
-                    st.write(mean)
-                    st.write(var)
+                for idx, val in mids.iterrows():
+                    block = val['block']
                     miles = mileages[block]
-                    x = val/miles
-                    st.write(x)
-                    prob = norm.ppf(x, mean, var)
-                    st.write(prob)
-                # break
+                    kwhs_to_go = 440 * (current_soc - 0.2)  # kwhs left in tank
+                    eff = kwhs_to_go / miles  # eff
+                    prob = norm.cdf(eff, val['pred_eff'], val['var'] ** 0.5)
+                    prob_data = prob_data.append(
+                        {'coach': coach, 'block': block,
+                         'Probability of Block Completion': "{}%".format(int(prob * 100)), 'prob': prob},
+                        ignore_index=True)
+                prob_data = prob_data.sort_values('block', ascending=True)
 
-                # st.write(variance.mean())
-                # TODO: add in SOC to the graphs/calculations
-                # TODO: Get Probs
+
+                def highlight_cells(val, color_if_true, color_if_false):
+                    color = color_if_true if val >= 0.95 else color_if_false
+
+
+                # prob_data.prob = prob_data.style.applymap(highlight_cells, color_if_true='green',
+                # color_if_false='red', subset=['prob']) st.dataframe(prob_data['Probability of Block Completion'],
+                # use_container_width=True)
+                coach_prop_data = prob_data[prob_data['coach'] == coach]
+                coach_prop_data = coach_prop_data[['block', 'Probability of Block Completion']].set_index('block')
+                st.dataframe(coach_prop_data, use_container_width=True)
+                # output = data.sort_values('block', ascending=True)
                 # TODO: rewrite table
                 # TODO: add in optimization predictions
+
+    st.markdown("# Optimization Predictions")
+    st.write("To be added shortly")
+    probabilities = prob_data[['coach', 'block', 'prob']]
+    probabilities['coach'] = probabilities['coach'].astype(str)
+    probabilities['block'] = probabilities['block'].astype(str)
+    probabilities = probabilities.set_index(['coach', 'block'])
+
+    st.markdown("## Assignments: ")
+    st.markdown("Assuming probablility threshold of 95%")
+    assignments = solve(probabilities, ebec_input, prob_data)
+    st.write(assignments)
+    assignments = assignments.reset_index()
+    assignments = assignments.merge(prob_data, left_on=['index', 'Block'], right_on=['coach', 'block'], how='left')
+    st.dataframe(assignments.set_index('index')[['Block', 'Probability of Block Completion']].sort_index(),
+                 use_container_width=True)
+
+    # st.markdown("## Charging Assignments: ")
+    # st.write("Make assignments for charging in order to the buses back on track")
+
+with tab2:
+    st.markdown("## Solar LSTM/prophet")
+    st.markdown("## Form for user input")
+    st.markdown("## Charging Schedule for week")
+    st.write("Optimal Cost: {}".format(10))
+    st.markdown("")
