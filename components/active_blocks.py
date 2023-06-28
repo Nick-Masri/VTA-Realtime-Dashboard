07575aa313df6bc3e0a1e188de0919d8353fd3e2
Nick Masri <nicholasmasri@gmail.com>
2023-06-28 13:04:21 -0700

    fixed out on time routes and error from streamlit selection

--- components/active_blocks.py
+++ components/active_blocks.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from supabase import create_client, Client
 import os
 
-
 def get_active_blocks():
     # Fetch data from API
     url = "https://api.goswift.ly/real-time/vta/active-blocks"
@@ -16,44 +15,38 @@ def get_active_blocks():
     # Extract relevant data and create DataFrame
     block_data = json_data["data"]["blocksByRoute"]
     df = pd.DataFrame(block_data)
-    old_buses = [f'750{x}' for x in range(1, 6)]
-    new_buses = [f'950{x}' for x in range(1, 6)]
-    ebuses = old_buses + new_buses
-    df = df[df.id.isin(ebuses)]
+
+    # Explode "block" column into separate rows
+    exploded_df = df.explode("block")
+    exploded_df = exploded_df.reset_index(drop=True)
+    block_df = pd.DataFrame(exploded_df["block"].to_list())
+    block_df = block_df.add_prefix("block_")
+
+    # Concatenate exploded DataFrame and block DataFrame
+    df = pd.concat([exploded_df.drop("block", axis=1), block_df], axis=1)
+
+    # Expand the 'block_vehicle' column
+    expanded_df = df.explode('block_vehicle')
+    expanded_df = expanded_df[['id', 'block_id', 'block_startTime', 'block_endTime', 'block_vehicle']]
+    # st.write(expanded_df)
+    # Extract vehicle details from 'block_vehicle' column
+    vehicle_df = expanded_df.block_vehicle.apply(pd.Series)
+    vehicle_df = vehicle_df.rename(columns={"id": "coach"})
+
+    # Concatenate expanded DataFrame and vehicle DataFrame
+    df = pd.concat([expanded_df, vehicle_df], axis=1)
+    # st.write(df)
+    df = df[
+        ['id', 'block_id', 'block_startTime', 'block_endTime', 'coach', 'isPredictable', 'schAdhSecs']]
 
     if len(df) > 0:
-
-        # Explode "block" column into separate rows
-        exploded_df = df.explode("block")
-        exploded_df = exploded_df.reset_index(drop=True)
-        block_df = pd.DataFrame(exploded_df["block"].to_list())
-        block_df = block_df.add_prefix("block_")
-
-        # Concatenate exploded DataFrame and block DataFrame
-        final_df = pd.concat([exploded_df.drop("block", axis=1), block_df], axis=1)
-
-        # Expand the 'block_vehicle' column
-        expanded_df = final_df.explode('block_vehicle')
-        expanded_df = expanded_df[['id', 'block_id', 'block_startTime', 'block_endTime', 'block_vehicle']]
-
-        # Extract vehicle details from 'block_vehicle' column
-        vehicle_df = expanded_df.block_vehicle.apply(pd.Series)
-        vehicle_df = vehicle_df.rename(columns={"id": "coach"})
-
-        # Concatenate expanded DataFrame and vehicle DataFrame
-        merged_df = pd.concat([expanded_df, vehicle_df], axis=1)
-        merged_df = merged_df[
-            ['id', 'block_id', 'block_startTime', 'block_endTime', 'coach', 'isPredictable', 'schAdhSecs']]
-
-        # Convert 'block_endTime' column to datetime format
-        merged_df['block_endTime'] = pd.to_datetime(merged_df['block_endTime'], errors='coerce')
-
-        # Calculate 'predictedArrival' by adding 'block_endTime' and 'schAdhSecs' columns
-        merged_df['predictedArrival'] = merged_df['block_endTime'] + pd.to_timedelta(merged_df['schAdhSecs'], unit='s')
-
-        merged_df.drop(columns=['isPredictable', 'schAdhSecs'], inplace=True)
-
-        return merged_df.copy()
+        df['block_endTime'] = pd.to_datetime(df['block_endTime'], errors='coerce')
+        df['predictedArrival'] = df['block_endTime'] + pd.to_timedelta(df['schAdhSecs'], unit='s')
+        df.drop(columns=['isPredictable', 'schAdhSecs'], inplace=True)
+        ebuses = [f'750{i}' for i in range(1, 6)] + [f'950{i}' for i in range(1, 6)]
+        df = df[df.coach.isin(ebuses)]
+        # st.write(df)
+        return df.copy()
     else:
         # grab from supabase
         # Load environment variables
@@ -79,14 +72,14 @@ def show_active_blocks(merged_df=get_active_blocks()):
     if len(merged_df) > 0:
         st.subheader("Out on Routes")
         st.caption("Predicted Arrival Time from Swiftly")
-
+        # st.write(merged_df)
         # Display the DataFrame
         merged_df = merged_df[
-            ['coach', 'id', 'block_id', 'block_startTime', 'block_endTime', 'predictedArrival', 'soc']]
+            ['coach', 'id', 'block_id', 'block_startTime', 'block_endTime', 'predictedArrival', 'soc', 'last_transmission']]
 
     st.dataframe(merged_df, hide_index=True,
                  column_order=['coach', 'soc', 'id', 'block_id', 'block_startTime', 'block_endTime',
-                               'predictedArrival'],
+                               'predictedArrival', 'last_transmission'],
                  column_config={
                      "coach": st.column_config.TextColumn("Coach"),
                      "id": st.column_config.TextColumn("Route"),
