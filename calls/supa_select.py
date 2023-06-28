c536ce37d8a27bff1181dbf2ec32673e67697907
Nick Masri <nicholasmasri@gmail.com>
2023-06-28 14:04:47 -0700

    changed supabase history call

--- calls/supa_select.py
+++ calls/supa_select.py
@@ -5,14 +5,16 @@ import os
 from datetime import datetime
 import pytz
 
-def supabase_block_history():
+def setup_client():
     # (if local) Load environment variables
     load_dotenv()
-
     url: str = os.environ.get("SUPABASE_URL")
     key: str = os.environ.get("SUPABASE_KEY")
     supabase: Client = create_client(url, key)
+    return supabase
 
+def supabase_block_history():
+    supabase = setup_client()
     response = supabase.table('block_history').select("*").execute()
     data = response.data
     df = pd.DataFrame(data).drop(columns='id')
@@ -27,11 +29,7 @@ def supabase_block_history():
         return None
 
 def supabase_soc():
-    # (if local) Load environment variables
-    load_dotenv()
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_KEY")
-    supabase: Client = create_client(url, key)
+    supabase = setup_client()
     yesterday = datetime.today() - pd.Timedelta(days=1)
     response = supabase.table('soc').select("*").gt("created_at", yesterday).execute()
     data = response.data
@@ -47,6 +45,21 @@ def supabase_soc():
     # st.write(df)
     # Format the odometer column with thousands separator
     df['odometer'] = df['odometer'].apply(lambda x: "{:,}".format(x))
+
+    # Convert last_transmission column to California timezone
+    california_tz = pytz.timezone('America/Los_Angeles')
+    df['last_transmission'] = pd.to_datetime(df['last_transmission']).dt.tz_convert(california_tz)
+    return df.copy()
+
+def supabase_soc_history():
+    supabase = setup_client()
+    yesterday = datetime.today() - pd.Timedelta(days=1)
+    response = supabase.table('soc').select("*").order("created_at", desc=True).execute()
+    data = response.data
+    df = pd.DataFrame(data)
+    df['vehicle'] = df['vehicle'].astype(str)
+    df['created_at'] = pd.to_datetime(df['created_at'])
+    df.sort_values(by='created_at', ascending=False, inplace=True)
 
     # Convert last_transmission column to California timezone
     california_tz = pytz.timezone('America/Los_Angeles')
