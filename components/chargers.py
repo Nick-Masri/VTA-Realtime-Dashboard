0dde0fdba392a8a09f32b723fd6a864f764bf045
Nick Masri <nicholasmasri@gmail.com>
2023-07-01 13:27:16 -0700

    fix main error with no active chargers

--- components/chargers.py
+++ components/chargers.py
@@ -15,40 +15,45 @@ def show_chargers():
     # do these link up to vehicle? I guess we will see
     df["stationName"] = df["stationName"].str.replace(' / ', ' ')
     active = df[df['Charging'] == True]
-    active['startTime'] = pd.to_datetime(active['startTime'])
-    active['startTime'] = active['startTime'].dt.tz_convert('US/Pacific')
     inactive = df[df['Charging'] == False]
-    st.subheader("Currently Charging")
-    st.dataframe(active, hide_index=True, use_container_width=True,
-                    column_order=[
-                        "stationName","startTime", "startBatteryPercentage", "Energy", 
-                                   "totalChargingDuration", "totalSessionDuration",
-                                      ],
-                    column_config={
-                        "stationName": st.column_config.TextColumn("Station Name"),
-                        "startTime": st.column_config.DatetimeColumn("Start Time",
-                                                                        format="MM/DD/YY hh:mmA"),
-                        "totalChargingDuration": st.column_config.TextColumn("Charging Duration"),
-                        "totalSessionDuration": st.column_config.TextColumn("Session Duration"),
-                        "startBatteryPercentage": st.column_config.ProgressColumn("Start SOC (%)",
-                                                                                format='%d%%',
-                                                                                min_value=0,max_value=100),
-                        "stopBatteryPercentage": st.column_config.ProgressColumn("End SOC (%)",
-                                                                                format='%d%%',
-                                                                                min_value=0,max_value=100),
-                        "Energy": st.column_config.NumberColumn("Energy (kWh)"),
-                        "endedBy": st.column_config.TextColumn("Ended By"),
-                    })
-    
-    st.subheader("Inactive ")
-    st.dataframe(inactive, hide_index=True, use_container_width=True,
-                    column_order=[
-                    "stationName", 
-                    "Status",
-                    "networkStatus",
-                     ], 
-                     column_config={
-                        "stationName": st.column_config.TextColumn("Station Name"),
-                        "networkStatus": st.column_config.TextColumn("Network Status")
+
+    if not active.empty:
+        active['startTime'] = pd.to_datetime(active['startTime'])
+        active['startTime'] = active['startTime'].dt.tz_convert('US/Pacific')
+        st.subheader("Currently Charging")
+        st.dataframe(active, hide_index=True, use_container_width=True,
+                        column_order=[
+                            "stationName","startTime", "startBatteryPercentage", "Energy", 
+                                    "totalChargingDuration", "totalSessionDuration",
+                                        ],
+                        column_config={
+                            "stationName": st.column_config.TextColumn("Station Name"),
+                            "startTime": st.column_config.DatetimeColumn("Start Time",
+                                                                            format="MM/DD/YY hh:mmA"),
+                            "totalChargingDuration": st.column_config.TextColumn("Charging Duration"),
+                            "totalSessionDuration": st.column_config.TextColumn("Session Duration"),
+                            "startBatteryPercentage": st.column_config.ProgressColumn("Start SOC (%)",
+                                                                                    format='%d%%',
+                                                                                    min_value=0,max_value=100),
+                            "stopBatteryPercentage": st.column_config.ProgressColumn("End SOC (%)",
+                                                                                    format='%d%%',
+                                                                                    min_value=0,max_value=100),
+                            "Energy": st.column_config.NumberColumn("Energy (kWh)"),
+                            "endedBy": st.column_config.TextColumn("Ended By"),
                         })
-    chargepoint_map(stations)
+        
+    if not inactive.empty:
+        st.subheader("Inactive ")
+        st.dataframe(inactive, hide_index=True, use_container_width=True,
+                        column_order=[
+                        "stationName", 
+                        "Status",
+                        "networkStatus",
+                        ], 
+                        column_config={
+                            "stationName": st.column_config.TextColumn("Station Name"),
+                            "networkStatus": st.column_config.TextColumn("Network Status")
+                            })
+        
+    if not stations.empty:
+        chargepoint_map(stations)
