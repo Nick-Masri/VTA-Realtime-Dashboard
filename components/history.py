8ef0a7df64fabbb45406327e68e3a37b85ad3c76
Nick Masri <nicholasmasri@gmail.com>
2023-07-01 14:19:21 -0700

    renaming on history tab

--- components/history.py
+++ components/history.py
@@ -5,8 +5,8 @@ def show_history():
     # Route History
     st.write("## History")
     # select vehicle or charger history
-    selection = st.selectbox("Select History", ["Vehicles", "Chargers"])
-    if selection == "Chargers":
+    selection = st.selectbox("Select History", ["Block Drive History", "Charging History"])
+    if selection == "Charging History":
         show_charger_history()
-    elif selection == "Vehicles":
+    elif selection == "Block Drive History":
         show_vehicle_history()
