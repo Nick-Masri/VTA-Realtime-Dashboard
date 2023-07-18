3f334412703841521b93d31184040c43a276630b
Nick Masri <nicholasmasri@gmail.com>
2023-07-17 18:39:06 -0700

    removed api key and login from main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import streamlit as st
-# from streamlit_supabase_auth import login_form, logout_button
-
 from page_files.dashboard import dashboard
 from page_files.history import show_history
 from page_files.vehicles import show_vehicles
@@ -13,24 +11,6 @@ from page_files.chargers import show_chargers
 
 def main():
     st.title("E-Bus Data Portal")
-    # login
-    # session = login_form(
-    #     url="https://ztkxpouaswqlomjpjnwl.supabase.co",
-    #     apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0a3hwb3Vhc3dxbG9tanBqbndsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY5MDczMTEsImV4cCI6MjAwMjQ4MzMxMX0.w6FazqiQ9BA1oSbPBf4MmXqeye7Y2U2b5Y2ERDWdcA8",
-    #     # providers=["google"]
-    # )
-    #
-    # if session:
-    #     # login stuffvta
-    #     st.experimental_set_query_params(page=["success"])
-    #
-    #     with st.sidebar:
-    #         st.write(f"Welcome {session['user']['email']}")
-    #         logout_button(
-    #             url="https://ztkxpouaswqlomjpjnwl.supabase.co",
-    #             apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0a3hwb3Vhc3dxbG9tanBqbndsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY5MDczMTEsImV4cCI6MjAwMjQ4MzMxMX0.w6FazqiQ9BA1oSbPBf4MmXqeye7Y2U2b5Y2ERDWdcA8",
-    #             # providers=["google"]
-    #         )
 
     dash, vehicles, chargers, hist = st.tabs(["Dashboard", 
                                                         # "Location", 
