3a0211c68d54f0656b7f43695cac71c95919301e
Nick Masri <nicholasmasri@gmail.com>
2023-07-18 00:21:34 -0700

    moved location and slight semantic change

--- page_files/chargers.py
+++ page_files/chargers.py
@@ -22,6 +22,7 @@ def show_chargers():
     if not active.empty:
         active = active.copy()
         active['vehicle'] = active['vehiclePortMAC'].map(data.mac_to_name)
+        active['vehicle'] = active['vehicle'].fillna('Unknown')k
 
         active['startTime'] = pd.to_datetime(active['startTime'])
         active['startTime'] = active['startTime'].dt.tz_convert('US/Pacific')
