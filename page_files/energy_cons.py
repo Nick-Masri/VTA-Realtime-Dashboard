fc95bc9cd8264ad2bd3460619827e9b63c459b8d
aas620 <aas620@lehigh.edu>
2023-11-02 16:10:36 -0400

    page_files edit

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -6,12 +6,12 @@ def show_energy_cons():
     st.write("Here we will load our model saved model")
     st.write("Then we will add buttons for user input")
 
-    vehicle_options = [f'750{x}' for x in range(1, 6)] + [f'950{x}' for x in range(1, 6)]
+    #vehicle_option = [f'750{x}' for x in range(1, 6)] + [f'950{x}' for x in range(1, 6)]
 
-    vehicle = st.selectbox('Select a vehicle', vehicle_options)
+    #vehicles = st.selectbox('Select a vehicle', vehicle_option)
 
-    #block_option = [7770,7774,7773,7772,7771,7776,7775,7777,9883,9882,9983, 9982,9984,9986,9985,9987,7777071,7777072]
-    #block = st.selectbox('Select block', block_option)
+    block_option = [7770,7774,7773,7772,7771,7776,7775,7777,9883,9882,9983, 9982,9984,9986,9985,9987,7777071,7777072]
+    block = st.selectbox('Select block', block_option)
     st.text_input('Number of miles left to be travelled:')
     st.text_input('Current temperature:')
     st.text_input('Cloud cover:')
