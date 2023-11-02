c9f985efc91949bcfd528fb4bc04fe84b6b21872
Matthias Culbertson <matthias.culbertson@gmail.com>
2023-11-02 16:06:11 -0400

    started work on consumption prediction model

---
+++ components/consumption_model.py
@@ -0,0 +1,10 @@
+from calls.visual_crossing import get_todays_weather
+import pickle
+
+
+
+def predict_consumption(block, coach):
+    weather_data = get_todays_weather()
+
+
+    pickle.load('../ML_Models/SOMETHING')
