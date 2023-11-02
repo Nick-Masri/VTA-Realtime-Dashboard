23c1e602f4fc160506f086d7bbe39a3270d72a3a
aas620 <aas620@lehigh.edu>
2023-11-02 16:11:19 -0400

    page_files edit

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -6,9 +6,9 @@ def show_energy_cons():
     st.write("Here we will load our model saved model")
     st.write("Then we will add buttons for user input")
 
-    #vehicle_option = [f'750{x}' for x in range(1, 6)] + [f'950{x}' for x in range(1, 6)]
+    vehicle_option = [f'750{x}' for x in range(1, 6)] + [f'950{x}' for x in range(1, 6)]
 
-    #vehicles = st.selectbox('Select a vehicle', vehicle_option)
+    vehicles = st.selectbox('Select a vehicle', vehicle_option)
 
     block_option = [7770,7774,7773,7772,7771,7776,7775,7777,9883,9882,9983, 9982,9984,9986,9985,9987,7777071,7777072]
     block = st.selectbox('Select block', block_option)
