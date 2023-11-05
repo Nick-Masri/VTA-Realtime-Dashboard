fd41d646c795a37504d6a1bc4af571fa569e361b
aas620 <aas620@lehigh.edu>
2023-11-05 17:56:48 -0500

    energy cons

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -3,39 +3,7 @@ import streamlit as st
 #import pickle
 import os
 
-# from calls.visual_crossing import get_todays_weather
-import pickle
-import requests #todo remove
-import numpy as np
-from datetime import date
-import warnings
-
-warnings.filterwarnings("ignore", category=UserWarning)
-
-#for debugging
-def get_todays_weather():
-    url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/santa%20clara/today?unitGroup=metric&include=days%2Ccurrent&key=3GYK8TN3A8NWKPGCYYW5S59CM&contentType=json'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        response_json = response.json()
-
-        desired_attribute = response_json['days'][0]
-    else:
-        raise Exception("Error retriving weather data")
-    
-    return desired_attribute
-
-
-def predict_consumption(block, coach, miles_travelled):
-    weather_data = get_todays_weather()
-    # today = date.today()
-    inputs = np.array([[block, weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], 4, 11, 2023, 1]])
-    model = pickle.load(open('./ML_models/energy_consumption_model.sav', 'rb'))
-    result = model.predict(inputs)[0]
-    return round(result,1)
-
-#print(predict_consumption(7773, 7505, 100))
+from components.consumption_model import predict_consumption
 def show_energy_cons():
     st.write("Here we will load our model saved model")
     st.write("Then we will add buttons for user input")
