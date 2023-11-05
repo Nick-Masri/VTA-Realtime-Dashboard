c92e8d7c3a3cfb97e54086648983e608d58d471c
aas620 <aas620@lehigh.edu>
2023-11-05 17:44:43 -0500

    testing directories

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -1,11 +1,13 @@
 #import pandas as pd
 import streamlit as st
 #import pickle
+import os
+
 from components.consumption_model import predict_consumption
 def show_energy_cons():
     st.write("Here we will load our model saved model")
     st.write("Then we will add buttons for user input")
-
+    
     voption = [2600, 7505, 7504, 7501, 2601, 2610, 2604, 2608, 2607, 2603, 2602,
        2606, 2605, 7503, 7502, 2609, 2612, 2611, 7506, 7507]
 
@@ -17,6 +19,7 @@ def show_energy_cons():
     #st.text_input('Current temperature:')
     #st.text_input('Cloud cover:')
     #st.text_input('Current SOC:')
+    st.write(os.getcwd())
     energy_used=predict_consumption(block, v, miles)
     st.button('Generate estimated battery remaining')
     st.write('The bus will use ' + energy_used + 'percent battery')
