c303a262e3947e281fca5b54b3b856fca4f69a7c
rec224 <rec224@lehigh.edu>
2023-11-05 16:11:32 -0500

    removed unnecessary params

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -13,10 +13,10 @@ def show_energy_cons():
 
     block_option = [7770,7774,7773,7772,7771,7776,7775,7777,9883,9882,9983, 9982,9984,9986,9985,9987,7777071,7777072]
     block = st.selectbox('Select block', block_option)
-    st.text_input('Number of miles left to be travelled:')
-    st.text_input('Current temperature:')
-    st.text_input('Cloud cover:')
-    st.text_input('Current SOC:')
+    #st.text_input('Number of miles left to be travelled:')
+    #st.text_input('Current temperature:')
+    #st.text_input('Cloud cover:')
+    #st.text_input('Current SOC:')
 
     st.button('Generate estimated battery remaining')
 
