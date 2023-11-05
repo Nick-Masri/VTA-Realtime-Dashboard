a935718729d1ce0db80e7b94ce536ff053260617
rec224 <rec224@lehigh.edu>
2023-11-05 17:05:26 -0500

    incorp energy model

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -1,7 +1,7 @@
 #import pandas as pd
 import streamlit as st
 #import pickle
-
+from components.consumption_model import predict_consumption
 def show_energy_cons():
     st.write("Here we will load our model saved model")
     st.write("Then we will add buttons for user input")
@@ -13,13 +13,13 @@ def show_energy_cons():
 
     block_option = [7770,7774,7773,7772,7771,7776,7775,7777,9883,9882,9983, 9982,9984,9986,9985,9987,7777071,7777072]
     block = st.selectbox('Select block', block_option)
-    #st.text_input('Number of miles left to be travelled:')
+    miles = st.text_input('Number of miles left to be travelled:')
     #st.text_input('Current temperature:')
     #st.text_input('Cloud cover:')
     #st.text_input('Current SOC:')
-
+    energy_used=predict_consumption(block, v, miles)
     st.button('Generate estimated battery remaining')
-
+    st.write('The bus will use ' + energy_used + 'percent battery')
     st.success('Bus will be able to travel route without needing to recharge')
 
     
