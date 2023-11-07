a224c4edaeef62c67d10a9eecb46b2d0d099993d
Matthias Culbertson <matthias.culbertson@gmail.com>
2023-11-06 20:37:06 -0500

    more issues with deployment filepath

--- components/consumption_model.py
+++ components/consumption_model.py
@@ -5,6 +5,8 @@ import numpy as np
 from datetime import date
 import warnings
 import os
+from pathlib import Path
+
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -29,7 +31,8 @@ def predict_consumption(block, coach, miles_travelled):
     # inputs = np.array([[block, weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], 4, 11, 2023, 1]]).astype('uint')
     # inputs = np.array([[block, 67.5, coach, 43.2, miles_travelled, 14.5, 330.5, 8.9, 12.7, 34.1,0.0, 4, 11, 2023, 1]])
     inputs = np.array([[7773, 28.9, 7505, 65.3, 84, 12.2, 322, 14.9, 24, 34.5, 0.0, 23, 8, 2019,1]])
-    model = pickle.load(open('ML_models/energy_consumption_model.sav', 'rb'))
+    pkl_path = Path(__file__).parents[1] / 'ML_models/energy_consumption_model.sav'
+    model = pickle.load(open(pkl_path, 'rb'))
     result = model.predict(inputs)[0]
     return round(result,1)
 
