234d5215543984aa5f5bf16908a41abb4170deca
Matthias Culbertson <matthias.culbertson@gmail.com>
2023-11-06 20:16:20 -0500

    fixed filepath issue

--- components/consumption_model.py
+++ components/consumption_model.py
@@ -26,11 +26,10 @@ def get_todays_weather():
 def predict_consumption(block, coach, miles_travelled):
     weather_data = get_todays_weather()
     # today = date.today()
-    inputs = np.array([[block, weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], 4, 11, 2023, 1]])
-    try:
-        model = pickle.load(open('../ML_models/energy_consumption_model.sav', 'rb'))
-    except:
-        raise Exception("working dir is" + os.getcwd() )
+    # inputs = np.array([[block, weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], 4, 11, 2023, 1]]).astype('uint')
+    # inputs = np.array([[block, 67.5, coach, 43.2, miles_travelled, 14.5, 330.5, 8.9, 12.7, 34.1,0.0, 4, 11, 2023, 1]])
+    inputs = np.array([[7773, 28.9, 7505, 65.3, 84, 12.2, 322, 14.9, 24, 34.5, 0.0, 23, 8, 2019,1]])
+    model = pickle.load(open('ML_models/energy_consumption_model.sav', 'rb'))
     result = model.predict(inputs)[0]
     return round(result,1)
 
