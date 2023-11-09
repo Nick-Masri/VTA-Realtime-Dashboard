cd686d1c79502690bf2c001ed240c46e9a198bea
Matthias Culbertson <matthias.culbertson@gmail.com>
2023-11-09 15:24:04 -0500

    modified to print file path

--- components/consumption_model.py
+++ components/consumption_model.py
@@ -32,9 +32,11 @@ def predict_consumption(block, coach, miles_travelled):
     # inputs = np.array([[block, 67.5, coach, 43.2, miles_travelled, 14.5, 330.5, 8.9, 12.7, 34.1,0.0, 4, 11, 2023, 1]])
     inputs = np.array([[7773, 28.9, 7505, 65.3, 84, 12.2, 322, 14.9, 24, 34.5, 0.0, 23, 8, 2019,1]])
     pkl_path = Path(__file__).parents[1] / 'ML_models/energy_consumption_model.sav'
-    pkl_path = 'energy_consumption_model.sav'
-    model = pickle.load(open(pkl_path, 'rb'))
-    result = model.predict(inputs)[0]
-    return round(result,1)
+    return pkl_path
+    # pkl_path = 'ML_models/energy_consumption_model.sav'
+    # pkl_path = os.path.join(os.getcwd(), 'energy_consumption_model.sav')
+    # model = pickle.load(open(pkl_path, 'rb'))
+    # result = model.predict(inputs)[0]
+    # return round(result,1)
 
 print(predict_consumption(7773, 7505, 100))
