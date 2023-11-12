3dc60b24ca1de9ae85ab67349f252610d964dd0c
aas620 <aas620@lehigh.edu>
2023-11-12 16:49:26 -0500

    got rid of float type error

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -29,16 +29,19 @@ def get_todays_weather():
 def predict_consumption(block, coach, miles_travelled):
     weather_data = get_todays_weather()
     # today = date.today()
-    inputs = np.array([[block, weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], 4, 11, 2023, 1]])
+    if miles_travelled != '':
+        inputs = np.array([[block, weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], 4, 11, 2023, 1]])
     # inputs = np.array([[block, 67.5, coach, 43.2, miles_travelled, 14.5, 330.5, 8.9, 12.7, 34.1,0.0, 4, 11, 2023, 1]])
     #inputs = np.array([[7773, 28.9, 7505, 65.3, 84, 12.2, 322, 14.9, 24, 34.5, 0.0, 23, 8, 2019,1]])
-    pkl_path = './ML_models/energy_consumption_model.sav'
+        pkl_path = './ML_models/energy_consumption_model.sav'
     # return pkl_path
     # pkl_path = 'ML_models/energy_consumption_model.sav'
     # pkl_path = os.path.join(os.getcwd(), 'energy_consumption_model.sav')
-    model = pickle.load(open(pkl_path, 'rb'))
-    result = model.predict(inputs)[0]
-    return round(result,1)
+        model = pickle.load(open(pkl_path, 'rb'))
+        result = model.predict(inputs)[0]
+        return round(result,1)
+    else:
+        return -1
 
 def show_energy_cons():
     #st.write("Here we will load our model saved model")
@@ -56,8 +59,13 @@ def show_energy_cons():
     #st.text_input('Cloud cover:')
     #st.text_input('Current SOC:')
     energy_used=predict_consumption(block, v, miles)
+    st.button('Generate estimated battery remaining')
+    if energy_used is not None and energy_used != -1:
+        st.write('The amount of energy the bus uses in the route is ' + str(energy_used) + '%')
+    else:
+        st.warning('Please enter the number of miles.')
     #st.button('Generate estimated battery remaining')
    #  st.write('The bus will use ' + energy_used + 'percent battery')
-    st.write('The amount of energy the bus uses in the route is ' + str(energy_used) + '%')
+    #st.write('The amount of energy the bus uses in the route is ' + str(energy_used) + '%')
     #st.success('Bus will be able to travel route without needing to recharge')
 
