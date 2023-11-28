1649e52df7b6995641b2f2de92256aa4ad5428af
Matthias Culbertson <matthias.culbertson@gmail.com>
2023-11-28 15:51:44 -0500

    changed blocks to strings

--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -7,11 +7,13 @@ warnings.filterwarnings("ignore", category=UserWarning)
 def show_energy_cons():
     voption = [2600, 7505, 7504, 7501, 2601, 2610, 2604, 2608, 2607, 2603, 2602,
        2606, 2605, 7503, 7502, 2609, 2612, 2611, 7506, 7507]
+    
+    # block_to_miles_dictionary = {"7770":,"7774","7773","7772","7771","7776","7775","7777","9883","9882","9983", "9982","9984","9986","9985","9987"}
 
     v = st.selectbox('Select a vehicle', voption)
     startSOC = st.text_input('Input  the current bus SOC') #TODO make str to float conversion more robust
-    block_option = [7770,7774,7773,7772,7771,7776,7775,7777,9883,9882,9983, 9982,9984,9986,9985,9987]
-    block = st.selectbox('Select block', block_option)
+    block_option = ["7770","7774","7773","7772","7771","7776","7775","7777","9883","9882","9983", "9982","9984","9986","9985","9987"]
+    block = int(st.selectbox('Select block', block_option))
     miles = st.text_input('Number of miles left to be travelled:')
     energy_used, probability = predict_consumption(block, v, miles, 0 if startSOC=='' else float(startSOC))
     st.button('Generate estimated energy used')
