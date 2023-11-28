4758d785dbf71b33dd6a13be483cd0938999ba8e
Matthias Culbertson <matthias.culbertson@gmail.com>
2023-11-28 15:17:34 -0500

    cleaned up energy cons code

--- components/consumption_model.py
+++ components/consumption_model.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-from datetime import date #TODO grab date
+from datetime import date
 import warnings
 from scipy.stats import t
 from calls.visual_crossing import get_todays_weather
@@ -10,15 +10,10 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 def predict_consumption(block, coach, miles_travelled, percent):
     weather_data = get_todays_weather()
-    # today = date.today()
+    today = date.today()
     if miles_travelled != '':
-        inputs = np.array([[weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], 4, 11]])
-    # inputs = np.array([[block, 67.5, coach, 43.2, miles_travelled, 14.5, 330.5, 8.9, 12.7, 34.1,0.0, 4, 11, 2023, 1]])
-    #inputs = np.array([[7773, 28.9, 7505, 65.3, 84, 12.2, 322, 14.9, 24, 34.5, 0.0, 23, 8, 2019,1]])
+        inputs = np.array([[weather_data['cloudcover'], coach, weather_data['humidity'], miles_travelled, weather_data['visibility'], weather_data['winddir'], weather_data['windspeed'], weather_data['feelslikemin'], weather_data['solarradiation'], weather_data['precipcover'], today.month, today.date]])
         pkl_path = './ML_models/mapie_energy_consumption_model.sav'
-    # return pkl_path
-    # pkl_path = 'ML_models/energy_consumption_model.sav'
-    # pkl_path = os.path.join(os.getcwd(), 'energy_consumption_model.sav')
         model = pickle.load(open(pkl_path, 'rb'))
         a = 0.01
         deg_free = 8
--- page_files/energy_cons.py
+++ page_files/energy_cons.py
@@ -5,21 +5,15 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
 def show_energy_cons():
-    #st.write("Here we will load our model saved model")
-    #st.write("Then we will add buttons for user input")
-    
     voption = [2600, 7505, 7504, 7501, 2601, 2610, 2604, 2608, 2607, 2603, 2602,
        2606, 2605, 7503, 7502, 2609, 2612, 2611, 7506, 7507]
 
     v = st.selectbox('Select a vehicle', voption)
-    startSOC = st.text_input('Input  the current bus SOC')
+    startSOC = st.text_input('Input  the current bus SOC') #TODO make str to float conversion more robust
     block_option = [7770,7774,7773,7772,7771,7776,7775,7777,9883,9882,9983, 9982,9984,9986,9985,9987]
     block = st.selectbox('Select block', block_option)
     miles = st.text_input('Number of miles left to be travelled:')
-    #st.text_input('Current temperature:')
-    #st.text_input('Cloud cover:')
-    #st.text_input('Current SOC:')
-    energy_used, probability = predict_consumption(block, v, miles, 0 if startSOC is None else float(startSOC))
+    energy_used, probability = predict_consumption(block, v, miles, 0 if startSOC=='' else float(startSOC))
     st.button('Generate estimated energy used')
     if energy_used is not None and energy_used != -1 and startSOC is not None:
         st.write('The amount of energy the bus uses in the route is ' + str(energy_used) + '%')
@@ -31,8 +25,4 @@ def show_energy_cons():
             st.write('The bus will not be able to complete the route.')
     else:
         st.warning('Please enter the number of miles.')
-    #st.button('Generate estimated battery remaining')
-   #  st.write('The bus will use ' + energy_used + 'percent battery')
-    #st.write('The amount of energy the bus uses in the route is ' + str(energy_used) + '%')
-    #st.success('Bus will be able to travel route without needing to recharge')
 
